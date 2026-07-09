d60c91da2469eec39c8d3b6376b12272fb7947fc
Benjamin Pabst <166110368+BenjaminPabst@users.noreply.github.com>
2026-07-09 13:00:27 +0200

    fix: use correct user id

--- src/app/chat/pages/chat-assistant/chat-assistant.effects.ts
+++ src/app/chat/pages/chat-assistant/chat-assistant.effects.ts
@@ -63,9 +63,9 @@ export class ChatAssistantEffects implements OnDestroy {
 
   loadUserProfile$ = createEffect(() => {
     return this.userService.profile$.pipe(
-      filter((profile) => !!profile?.person?.email),
-      map(({ person }) => {
-        const user = person.email as string;
+      filter((profile) => !!profile?.userId),
+      map((profile) => {
+        const user = profile.userId;
         return ChatAssistantActions.userProfileLoaded({ user });
       }),
     );
@@ -335,8 +335,9 @@ export class ChatAssistantEffects implements OnDestroy {
         this.store.select(chatAssistantSelectors.selectCurrentChat),
         this.store.select(chatAssistantSelectors.selectAgents),
         this.store.select(chatAssistantSelectors.selectSelectedAgentId),
+        this.store.select(chatAssistantSelectors.selectUser),
       ]),
-      switchMap(([action, chat, agents, selectedAgentId]) => {
+      switchMap(([action, chat, agents, selectedAgentId, user]) => {
         const selectedAgent = (agents as ChatAgent[]).find(
           (a) => a.id === (selectedAgentId as string),
         );
@@ -351,16 +352,17 @@ export class ChatAssistantEffects implements OnDestroy {
         return gather$.pipe(
           map(
             (context) =>
-              [action, chat, context, selectedAgent] as [
+              [action, chat, context, selectedAgent, user] as [
                 typeof action,
                 typeof chat,
                 (AiContextResponse | null)[],
                 ChatAgent | undefined,
+                typeof user,
               ],
           ),
         );
       }),
-      switchMap(([action, chat, context, selectedAgent]) => {
+      switchMap(([action, chat, context, selectedAgent, user]) => {
         if (!chat?.id || chat.id === 'new') {
           return of(
             ChatAssistantActions.createNewChatForMessage({
@@ -372,6 +374,7 @@ export class ChatAssistantEffects implements OnDestroy {
           .createChatMessage(chat.id, {
             type: MessageType.Human,
             text: action.message,
+            userId: user ?? '',
             awaitResponse: false,
             requestContext: {
               ...(selectedAgent?.filter
